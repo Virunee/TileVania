3406590218e811bf35c66462edd5a986c3d2bf3e
Virunee <viravongv@gmail.com>
2021-06-10 19:17:48 +0100

    Win screen
    Playing around with firework particle systems

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,20 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartFirstLevel()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
