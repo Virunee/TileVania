23298923d7702978c764bede341daae417c28ac3
Virunee <viravongv@gmail.com>
2021-08-06 11:22:48 +0100

    Create Scene Persistance
    Also fix some Cinemachine cameras

--- Assets/Scripts/ScenePersist.cs
+++ Assets/Scripts/ScenePersist.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScenePersist : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int startingSceneIdx;
+    private void Awake()
+    {
+        int numScenePersists = FindObjectsOfType<ScenePersist>().Length;
+        if (numScenePersists > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
     void Start()
     {
-        
+        startingSceneIdx = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int currentSceneIdx = SceneManager.GetActiveScene().buildIndex;
+        if (startingSceneIdx != currentSceneIdx)
+        {
+            Destroy(gameObject);
+        }
     }
 }
